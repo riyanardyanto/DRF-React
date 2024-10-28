4d4e4d7e0ff318b69475d3455e415bbaa947aaa8
riyanardyanto <riyanardyanto@gmail.com>
2024-10-29 06:53:43 +0700

    Implementing REACT-REDUX to fetch products & Redux-Devtools

--- frontend/ecommerce/src/components/screens/HomeScreen.js
+++ frontend/ecommerce/src/components/screens/HomeScreen.js
@@ -2,31 +2,49 @@ import React, { useEffect, useState } from "react"
 import { Container, Row, Col, Card } from "react-bootstrap"
 import axios from "axios"
 import Product from "../Product"
+import { listProducts } from "../../actions/productActions"
+import { useDispatch, useSelector } from "react-redux"
 
 function HomeScreen() {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
 
-  // get products
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
+
   useEffect(() => {
-    async function fetchProducts() {
-      const { data } = await axios.get("/api/products/")
-      setProducts(data)
-    }
+    dispatch(listProducts())
+  }, [dispatch])
 
-    fetchProducts()
-  }, [])
+  // const [products, setProducts] = useState([])
+
+  // // get products
+  // useEffect(() => {
+  //   async function fetchProducts() {
+  //     const { data } = await axios.get("/api/products/")
+  //     setProducts(data)
+  //   }
+
+  //   fetchProducts()
+  // }, [])
 
   return (
     <Container>
       <br />
       <h1>Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </Container>
   )
 }
